31564456063e4564792ac852105271e48d6c271c
Deepika Goyal <deepika.goyal0803@gmail.com>
2020-05-24 16:46:48 +0530

    minor changes in Landing.css

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -5,14 +5,13 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        <img className="wave" src="img/welwave1.png" alt=" " />
+        <img className="welwave" src="img/welwave1.png" alt=" " />
         <div className="welcontainer">
           <div className="jumbotron my-0 py-0 mx-0 px-0">
             <div className="col-sm-6  mx-auto my-auto">
               <br></br>
               <img className="logo" src="img/logo512.png" alt=" " />
               <h1 className="text-center">WELCOME TO WHATSWORK! </h1>
-              
             </div>
           </div>
         </div>
